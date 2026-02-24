3408d42840d0dfbcd5eef10d811d91dd6a55782b
Toshiki Togo <toshiki0221lol@gmail.com>
2026-02-25 00:42:53 +0900

    Implemented MTV cycle for store list

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Store  # 追記: Storeモデルをインポート
+
 # Create your views here.
+
+
+def store_list(request):
+    stores = Store.objects.all()
+    return render(request, "reservations/store_list.html", {"stores": stores})

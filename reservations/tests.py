1ad60594538bf684ef4aacc4919c6e8a1c773c31
Toshiki Togo <toshiki0221lol@gmail.com>
2026-03-10 01:36:02 +0900

    Add CI pipeline and basic tests

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Store
+
+
+class StoreModelTest(TestCase):
+    def test_store_creation(self):
+        """店舗（Store）が正しくデータベースに作成されるかをテストする"""
+        store = Store.objects.create(name="テストホテル博多", capacity=100)
+        self.assertEqual(store.name, "テストホテル博多")
+        self.assertEqual(store.capacity, 100)

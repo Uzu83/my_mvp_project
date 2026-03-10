055b38206a677df7318bd5322e63fa7b0bd6f1e3
Toshiki Togo <toshiki0221lol@gmail.com>
2026-03-10 23:31:13 +0900

    feat: add confirmation dialog and success message on cancel

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -58,5 +59,6 @@ def cancel_reservation(request, pk):
     if reservation.status == "RESERVED":
         reservation.status = "CANCELLED"
         reservation.save()
+        messages.success(request, "キャンセルしました")
 
     return redirect("reservations:my_reservations")
